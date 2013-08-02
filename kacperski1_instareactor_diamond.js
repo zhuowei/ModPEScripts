afe609e91b57cb64b8435a0e1c61689d0fd7e8b4
kacperski1 <kacperski1@gmail.com>
2013-08-03 01:31:54 +0200

    Update kacperski1_instareactor_diamond.js

--- kacperski1_instareactor_diamond.js
+++ kacperski1_instareactor_diamond.js
@@ -4,25 +4,25 @@ function useItem(x,y,z,itemId,blockId)
 {
   if(itemId == 264) // replace 264 with another item id if you want ;)
 	{
-		setTile(x,y,z,4);
-		setTile(x-1,y,z,4);
-		setTile(x+1,y,z,4);
-		setTile(x,y,z-1,4);
-		setTile(x,y,z+1,4);
-		setTile(x-1,y,z-1,41);
-		setTile(x-1,y,z+1,41);
-		setTile(x+1,y,z-1,41);
-		setTile(x+1,y,z+1,41);
-		setTile(x,y+1,z,247);
-		setTile(x-1,y+1,z-1,4);
-		setTile(x-1,y+1,z+1,4);
-		setTile(x+1,y+1,z-1,4);
-		setTile(x+1,y+1,z+1,4);
-		setTile(x,y+2,z,4);
-		setTile(x-1,y+2,z,4);
-		setTile(x+1,y+2,z,4);
-		setTile(x,y+2,z-1,4);
-		setTile(x,y+2,z+1,4);
+		setTile(x,y+1,z,4);
+		setTile(x-1,y+1,z,4);
+		setTile(x+1,y+1,z,4);
+		setTile(x,y+1,z-1,4);
+		setTile(x,y+1,z+1,4);
+		setTile(x-1,y+1,z-1,41);
+		setTile(x-1,y+1,z+1,41);
+		setTile(x+1,y+1,z-1,41);
+		setTile(x+1,y+1,z+1,41);
+		setTile(x,y+2,z,247);
+		setTile(x-1,y+2,z-1,4);
+		setTile(x-1,y+2,z+1,4);
+		setTile(x+1,y+2,z-1,4);
+		setTile(x+1,y+2,z+1,4);
+		setTile(x,y+3,z,4);
+		setTile(x-1,y+3,z,4);
+		setTile(x+1,y+3,z,4);
+		setTile(x,y+3,z-1,4);
+		setTile(x,y+3,z+1,4);
 		print("Nether Reactor created!");
 		
 		preventDefault();
