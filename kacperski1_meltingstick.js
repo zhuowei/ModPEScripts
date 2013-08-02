72337b7d29c959816ed9bc095bd2f29173c74a6b
kacperski1 <kacperski1@gmail.com>
2013-08-02 15:43:14 +0200

    Update kacperski1_meltingstick.js

--- kacperski1_meltingstick.js
+++ kacperski1_meltingstick.js
@@ -5,10 +5,11 @@ function useItem(x,y,z,itemId,blockId)
   if(blockId == 79)
 	{
 		setTile(x,y,z,8);
+		preventDefault();
 	}
 	else if(blockId == 49)
 	{
 		setTile(x,y,z,10);
+		preventDefault();
 	}
-	preventDefault();
 }
