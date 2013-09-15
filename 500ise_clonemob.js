23ae807601d13e06bf86c998ff75520d1df8a0de
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-09-15 13:20:32 -0400

    Clone mob: also clone age

--- 500ise_clonemob.js
+++ 500ise_clonemob.js
@@ -2,6 +2,10 @@ function attackHook(attacker, victim) {
 	var itemInHand = getCarriedItem();
 	if (itemInHand != 344) return; //egg
 	var victimType = Entity.getEntityTypeId(victim);
-	Entity.spawnMob(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), victimType, null);
+	var result = Entity.spawnMob(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), victimType, null);
+	if (victimType >= 10 && victimType < 14) { //animals
+		var oldAge = Entity.getAnimalAge(victim);
+		Entity.setAnimalAge(result, oldAge);
+	}
 	preventDefault();
 }
