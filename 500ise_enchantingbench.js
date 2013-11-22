b25e34b2f0d50ba0995434e03571562f1a3ebef8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-11-21 21:34:22 -0500

    Custom block demo script: enchanting bench

---
+++ 500ise_enchantingbench.js
@@ -0,0 +1,42 @@
+//example enchantment bench script by 500 Internal Server Error
+//uses block ID 187
+//use give commands to give yourself block 187; when tapped, it opens the crafting menu
+//this script is licensed under CC0: you may do whatever you want with it.
+var initialized = false;
+var ENCHANTMENT_TABLE_ID = 187;
+
+var currentWorkbenchX, currentWorkbenchY, currentWorkbenchZ;
+var needToSwitchBack = false;
+
+function selectLevelHook() { //This is called before the first world is loaded
+	if (!initialized) { //if we have not set up our custom block
+		// define a block with ID 187, name "Enchantment table", and the following textures
+		// in the order (bottom, top, south, north, west, east).
+		// in this case, the side textures are the same, and the top/bottom textures are different.
+		Block.defineBlock(ENCHANTMENT_TABLE_ID, "Enchantment table", [[2, 5], [10, 6], [11, 6], [11, 6], [11, 6], [11, 6]]);
+		// sets the block's boundaries.
+		// the order of parameters: xmax, xmin, zmax, zmin, ymax, ymin
+		// in this case, the block starts at y = 0, and stops at y = 0.75 - this is a block 3/4 m high.
+		Block.setShape(ENCHANTMENT_TABLE_ID, 0, 0, 0, 1, 3/4, 1);
+		Block.setDestroyTime(ENCHANTMENT_TABLE_ID, 1);
+		initialized = true;
+	}
+}
+
+function useItem(x, y, z, itemId, blockId, side) {
+	if (blockId == ENCHANTMENT_TABLE_ID) {
+		currentWorkbenchX = x;
+		currentWorkbenchY = y;
+		currentWorkbenchZ = z;
+		setTile(x, y, z, 58); //set to workbench so Minecraft PE would open the crafting menu
+		needToSwitchBack = true;
+	}
+}
+
+function modTick() {
+	if (needToSwitchBack) {
+		needToSwitchBack = false;
+		setTile(currentWorkbenchX, currentWorkbenchY, currentWorkbenchZ, ENCHANTMENT_TABLE_ID);
+		//switch it back to the original
+	}
+}
